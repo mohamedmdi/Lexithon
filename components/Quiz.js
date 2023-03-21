f1bfc33c6e3c6d4c8dcddac7cc11a4ae166b5a02
D-Noyti <mma.sipha13@gmail.com>
2023-03-21 15:19:05 +0000

    add sound

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,11 +1,36 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import useBackHandler from "../hooks/useBackHandler";
+import { Audio } from "expo-av";
 
 const Quiz = (props) => {
+  const [sound, setSound] = useState();
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const playSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/test.mp3")
+    );
+    setSound(sound);
+    await sound.playAsync();
+  };
 
   return (
     <View style={styles.sub}>
-        <Text>Hi {props.route.params.id}</Text>
+      <Text>Hi {props.route.params.id}</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Appuyez pour jouer le son</Text>
+        <TouchableOpacity onPress={playSound}>
+          <Text style={styles.button}>Jouer le son</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -45,5 +70,20 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "700",
     color: "#7c3aed",
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'blue',
+    color: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
   },
 });
