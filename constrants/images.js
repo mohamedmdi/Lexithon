53f91a016f543c8248544e875e5c0a7945379350
mohamedmdi <mohamed.mdi.sg@gmail.com>
2023-03-31 15:03:07 +0000

    Refactoring data V1

--- constrants/images.js
+++ constrants/images.js
@@ -1,16 +1,112 @@
-import apple from "../assets/imgs/apple.png"
-import orange from "../assets/imgs/orange.png"
-import juice from "../assets/imgs/juice.png"
-import cat from "../assets/imgs/cat.png"
-import chair from "../assets/imgs/chair.png"
-import table from "../assets/imgs/table.png"
-import man from "../assets/imgs/man.png"
-import coffee from "../assets/imgs/coffee.png"
-import moon from "../assets/imgs/moon.png"
-import car from "../assets/imgs/car.png"
+import apple from "../assets/imgs/apple.png";
+import orange from "../assets/imgs/orange.png";
+import juice from "../assets/imgs/juice.png";
+import cat from "../assets/imgs/cat.png";
+import chair from "../assets/imgs/chair.png";
+import table from "../assets/imgs/table.png";
+import man from "../assets/imgs/man.png";
+import coffee from "../assets/imgs/coffee.png";
+import moon from "../assets/imgs/moon.png";
+import car from "../assets/imgs/car.png";
+import apple from "../assets/imgs/apple.png";
+import orange from "../assets/imgs/orange.png";
+import juice from "../assets/imgs/juice.png";
+import cat from "../assets/imgs/cat.png";
+import chair from "../assets/imgs/chair.png";
+import table from "../assets/imgs/table.png";
+import man from "../assets/imgs/man.png";
+import coffee from "../assets/imgs/coffee.png";
+import moon from "../assets/imgs/moon.png";
+import car from "../assets/imgs/car.png";
+import ecole from "../assets/imgs/eco/ecole.png";
+import classe from "../assets/imgs/eco/classe.png";
+import porte from "../assets/imgs/eco/porte.png";
+import fenetre from "../assets/imgs/eco/fenetre.png";
+import tableau from "../assets/imgs/eco/tableau.png";
+import trousse from "../assets/imgs/eco/trousse.png";
+import gomme from "../assets/imgs/eco/gomme.png";
+import toilettes from "../assets/imgs/eco/toilettes.png";
+import jardin from "../assets/imgs/eco/jardin.png";
+import bureau from "../assets/imgs/eco/bureau.png";
+import banc from "../assets/imgs/eco/banc.png";
+import affiche from "../assets/imgs/eco/affiche.png";
+import ardoise from "../assets/imgs/eco/ardoise.png";
+import portail from "../assets/imgs/eco/portail.png";
+import livre from "../assets/imgs/eco/livre.png";
+import colle from "../assets/imgs/eco/colle.png";
+import eleve from "../assets/imgs/eco/eleve.png";
+import cour from "../assets/imgs/eco/cour.png";
+import table from "../assets/imgs/eco/table.png";
+import ciseaux from "../assets/imgs/eco/ciseaux.png";
+import chaise from "../assets/imgs/eco/chaise.png";
+import garcon from "../assets/imgs/eco/garcon.png";
+import cartable from "../assets/imgs/eco/cartable.png";
+import maitresse from "../assets/imgs/eco/maitresse.png";
+import fille from "../assets/imgs/eco/fille.png";
+import arbre from "../assets/imgs/eco/arbre.png";
+import ami from "../assets/imgs/eco/ami.png";
+import horloge from "../assets/imgs/eco/horloge.png";
+import calendrier from "../assets/imgs/eco/calendrier.png";
+import drapeau from "../assets/imgs/eco/drapeau.png";
+import projecteur from "../assets/imgs/eco/projecteur.png";
+import imprimante from "../assets/imgs/eco/imprimante.png";
+import brosse from "../assets/imgs/eco/brosse.png";
+import stylo from "../assets/imgs/eco/stylo.png";
+import ordinateur from "../assets/imgs/eco/ordinateur.png";
+import feuilles from "../assets/imgs/eco/feuilles.png";
+import regle from "../assets/imgs/eco/regle.png";
 
 const IMAGES = {
-    apple, orange, juice, cat, chair, table, man, coffee, moon, car
-}
+  apple,
+  orange,
+  juice,
+  cat,
+  chair,
+  table,
+  man,
+  coffee,
+  moon,
+  car,
+  ecole,
+  classe,
+  porte,
+  fenetre,
+  tableau,
+  trousse,
+  gomme,
+  toilettes,
+  jardin,
+  bureau,
+  banc,
+  affiche,
+  ardoise,
+  portail,
+  livre,
+  colle,
+  eleve,
+  cour,
+  table,
+  ciseaux,
+  chaise,
+  garcon,
+  cartable,
+  maitresse,
+  fille,
+  arbre,
+  ami,
+  stylo,
+  colle,
+  chaise,
+  horloge,
+  calendrier,
+  drapeau,
+  projecteur,
+  imprimante,
+  brosse,
+  stylo,
+  ordinateur,
+  feuilles,
+  regle,
+};
 
-export default IMAGES
+export default IMAGES;
